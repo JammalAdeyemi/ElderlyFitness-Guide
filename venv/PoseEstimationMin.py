a14e7a6cc9811a2a78ef758c6f713758e850ad52
Jammal <oabass7@gmail.com>
2023-07-15 12:29:53 +0100

    pose

--- venv/PoseEstimationMin.py
+++ venv/PoseEstimationMin.py
@@ -4,6 +4,7 @@ import time
 import math
 import matplotlib.pyplot as plt
 
+mpDraw = mp.solutions.drawing_utils
 mpPose = mp.solutions.pose
 pose = mpPose.Pose()
 
@@ -11,12 +12,25 @@ cap = cv2.VideoCapture('../Data/1.mp4')
 pTime = 0
 while True:
     success, img = cap.read()
-    
+    # Convert the BGR image to RGB.
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    # Perform Pose detection.
+    results = pose.process(imgRGB)
+    # Check for landmarks.
+    if results.pose_landmarks:
+        mpDraw.draw_landmarks(img, results.pose_landmarks, mpPose.POSE_CONNECTIONS)
+        # Print the landmarks.
+        for id, lm in enumerate(results.pose_landmarks.landmark):
+            h, w, c = img.shape
+            print(id, lm)
+            cx, cy = int(lm.x * w), int(lm.y * h)
+            cv2.circle(img, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
+            
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     pTime = cTime
     
     cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
     cv2.imshow("Image", img)
-    cv2.waitKey(10)
+    cv2.waitKey(1)
 
